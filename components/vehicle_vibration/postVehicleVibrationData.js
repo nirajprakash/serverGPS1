845b0bc0d5815efaeff0cf86a26457fc4e4390d4
nirajprakash <nirajprakash13@gmail.com>
2017-06-21 23:15:43 +0530

    from server

--- components/vehicle_vibration/postVehicleVibrationData.js
+++ components/vehicle_vibration/postVehicleVibrationData.js
@@ -6,7 +6,7 @@ var mModel = sequelize.import('../../models/vehicle_vibration.js');
 
 function postGPSData(req, res) {
     console.log(req.body);
-    res.send("True");
+    var error = false;
     /*
     EXPLANATION: 
         Here we recieve a post data - which is a json array in string from. 
@@ -21,14 +21,42 @@ function postGPSData(req, res) {
         to extract the relevant information associated together as a bundle.
 
     */
-    var vibrationData = {
-        data: JSON.stringify(req.body)
-    }
-    mModel.create(vibrationData)
+    var jsonArrayString = JSON.stringify(req.body)
+    var jsonArray = JSON.parse(jsonArrayString)
+    for (var i = jsonArray.length - 1; i >= 0; i--) {
+        var partsOfData = JSON.stringify(jsonArray[i].data).split(',');
+
+        console.log(partsOfData[14])
+        console.log(partsOfData[15].substring(0,partsOfData[15].length -1))
+
+        var vibrationData = {
+            userid : partsOfData[0].substring(1),
+            tripid : partsOfData[1],
+            username : partsOfData[2],
+            time : partsOfData[3],
+            vehicle : partsOfData[4],
+            accel_x: partsOfData[5],
+            accel_y: partsOfData[6],
+            accel_z: partsOfData[7],
+            gyro_x: partsOfData[8],
+            gyro_y: partsOfData[9],
+            gyro_z: partsOfData[10],
+            magnet_x: partsOfData[11],
+            magnet_y: partsOfData[12],
+            magnet_z: partsOfData[13],
+            latitude: partsOfData[14],
+            longitude: partsOfData[15].substring(0,partsOfData[15].length -1)
+        }
+        mModel.create(vibrationData)
         .asCallback(
         function (err, project) {
             if (err) {
                 console.log(err);
+                error =true;
             }
-        });    
+        }); 
+        res.send(!error);        
+    }
+       
 }
+
--- models/vehicle_vibration.js
+++ models/vehicle_vibration.js
@@ -1,15 +1,75 @@
 'use strict'
 
 module.exports = function (sequelize, Datatypes) {
-    return sequelize.define('vehicle_vibration', {
+    return sequelize.define('vehicleVibration', {
         id: {
             type: Datatypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
-        data: {
-            type: Datatypes.STRING(255),
+        userid: {
+            type: Datatypes.TEXT,
+            allowNull: false,
+            unique: false
+        },tripid: {
+            type: Datatypes.TEXT,
+            allowNull: false,
+            unique: false
+        },username: {
+            type: Datatypes.TEXT,
+            allowNull: false,
+            unique: false
+        },time: {
+            type: Datatypes.TEXT,
+            allowNull: false,
+            unique: false
+        },vehicle: {
+            type: Datatypes.TEXT,
+            allowNull: false,
+            unique: false
+        },accel_x: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },accel_y: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },accel_z: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },gyro_x: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },gyro_y: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },gyro_z: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },magnet_x: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },magnet_y: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },magnet_z: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },latitude: {
+            type: Datatypes.FLOAT,
+            allowNull: false,
+            unique: false
+        },longitude: {
+            type: Datatypes.FLOAT,
             allowNull: false,
             unique: false
         }
